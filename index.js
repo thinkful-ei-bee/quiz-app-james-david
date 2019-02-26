53e32d53973f64cff8e477f670d773247b30c201
David Bolin <david@bolin.at>
2019-02-26 09:47:33 -0800

    started js file and original variables and data

---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const questions = [];
+const answers = [];
+const rightAnswers = [];
+const STORE = {
+  view: 'startingPage',
+  currentQuestion: null,
+  currentResponse: null,
+  questionsComplete: 0,
+  currentCorrect: 0,
+};
+
+function main() {
+
+}
+
+$(main);
