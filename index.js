0ce626ecb52924829f436ff3b31227258a0c9be0
David Bolin <david@bolin.at>
2019-02-26 14:07:04 -0800

    removed some testing code

--- index.js
+++ index.js
@@ -33,31 +33,31 @@ const questions = [
 ];
 
 const answers = [
-  [`The statement causes an error`,
-  `doSomething runs if x is divisible by 3 or 5`,
-  `doSomething runs if x is divisible by 3 or 5, but not both`, 
-  `doSomething runs if x is divisible by both 3 and 5`
-], 
-  [`This returns x!, namely 1 * 2 * 3... * x`,
-  `This causes a reference error since magic is originally undefined`,
-  `This returns either 1 or undefined`,
-  `This returns NaN`
-], 
-  [`Undefined`,
-  `A new function that counts the number of times it has been called`,
-  `This causes an error`,
-  `1`
-], 
-  [`16.5`,
-  `34`,
-  `4.666...`,
-  `26`
-], 
-  [`This takes a string x and adds the letter 'i' after each occurrence of 'e'`,
-  `This takes a string x and replaces each letter 'e' with 'i'`,
-  `This takes a number x, divides by e and multiplies by i`,
-  `This causes a syntax error`
-],
+  ['The statement causes an error',
+    'doSomething runs if x is divisible by 3 or 5',
+    'doSomething runs if x is divisible by 3 or 5, but not both', 
+    'doSomething runs if x is divisible by both 3 and 5'
+  ], 
+  ['This returns x!, namely 1 * 2 * 3... * x',
+    'This causes a reference error since magic is originally undefined',
+    'This returns either 1 or undefined',
+    'This returns NaN'
+  ], 
+  ['Undefined',
+    'A new function that counts the number of times it has been called',
+    'This causes an error',
+    '1'
+  ], 
+  ['16.5',
+    '34',
+    '4.666...',
+    '26'
+  ], 
+  ['This takes a string x and adds the letter \'i\' after each occurrence of \'e\'',
+    'This takes a string x and replaces each letter \'e\' with \'i\'',
+    'This takes a number x, divides by e and multiplies by i',
+    'This causes a syntax error'
+  ],
 ];
 
 const rightAnswers = [2, 0, 1, 3, 1];
@@ -69,46 +69,69 @@ const STORE = {
   questionsComplete: 0,
   currentCorrect: 0,
 };
+console.log('beginning javascript file');
 
 function renderPage() {
-
-};
+  $('section#topOfApp').html(`<h1>Question 1: Lorem etc</h1>
+  <br>`);
+  $('form').html(`<input type="radio" name="answer" value="1" required>Answer 1</input><br>
+  <input type="radio" name="answer" value="2">Answer 2</input><br>
+  <input type="radio" name="answer" value="3">Answer 3</input><br>
+  <input type="radio" name="answer" value="4">Answer 4</input><br>
+  <button type="submit">Submit</button>`);
+}
 
 function handleSubmit() {
-
-};
+  $('form').on('click', 'button', function(event){
+    event.preventDefault();
+    console.log('button triggered');
+    console.log('Initial state:', STORE);
+    if (STORE.view === 'questionPage') {
+      let playerAnswer = $('input[name=answer]:checked').val() - 1;
+      STORE.questionsComplete += 1;
+      STORE.currentResponse = playerAnswer;
+      if (playerAnswer === rightAnswers[STORE.currentQuestion] ) {
+        STORE.currentCorrect += 1;
+      }
+    }
+    console.log('State after checking for question page', STORE);
+    advancePage();
+    console.log('State after advancing page', STORE);
+  });
+}
 
 function advancePage() {
   switch(STORE.view) {
-    case 'startingPage':
-      STORE.currentQuestion = 0,
-      STORE.view = 'questionPage';
-      break;
-    case 'questionPage':
-      STORE.view = 'answerPage';
-      break;
-    case 'answerPage':
-      if (STORE.currentQuestion === 4) {
-        STORE.view = 'lastPage';
-        STORE.currentQuestion = null;
-        STORE.currentResponse = null;  
-      }
-      else {STORE.view = 'questionPage';
+  case 'startingPage':
+    STORE.currentQuestion = 0,
+    STORE.view = 'questionPage';
+    break;
+  case 'questionPage':
+    STORE.view = 'answerPage';
+    break;
+  case 'answerPage':
+    if (STORE.currentQuestion === 4) {
+      STORE.view = 'lastPage';
+      STORE.currentQuestion = null;
+      STORE.currentResponse = null;  
+    }
+    else {STORE.view = 'questionPage';
       STORE.currentQuestion += 1;
       STORE.currentResponse = null;
-      }
-      break;
-    case 'lastPage':
-      STORE.view = 'startingPage';
-      STORE.currentQuestion = null;
-      STORE.currentResponse = null;
-      STORE.questionsComplete = 0;
-      STORE.currentCorrect = 0;
+    }
+    break;
+  case 'lastPage':
+    STORE.view = 'startingPage';
+    STORE.currentQuestion = null;
+    STORE.currentResponse = null;
+    STORE.questionsComplete = 0;
+    STORE.currentCorrect = 0;
   }
   renderPage();
-};
+}
 
 function main() {
+  console.log('starting main');
   renderPage(); 
   handleSubmit();
 }
